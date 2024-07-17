6fb3a171db26e44783b2cf8e68fb6b17c04c028b
facundo <alvarez.12facundo@gmail.com>
2024-07-17 18:24:20 -0300

    LoginRegister +Solucion de error

--- src/main/java/com/outsidethebox/project/controllers/MainController.java
+++ src/main/java/com/outsidethebox/project/controllers/MainController.java
@@ -28,23 +28,7 @@ public class MainController {
 		model.addAttribute("userInSession", usuarioProvisorio);
 		return "public/inicio-sesion.jsp";
 	}
-<<<<<<< HEAD
-	@GetMapping("/servicios")
-	public String servicios(Model model) {
-		model.addAttribute("userInSession", usuarioProvisorio);
-		model.addAttribute("pageTitle", "Servicios");
-		model.addAttribute("content", "/WEB-INF/servicios.jsp");
-		return "index.jsp";
-	}
-=======
 
-	@GetMapping("/registro")
-	public String signup(Model model) {
-		model.addAttribute("userInSession", usuarioProvisorio);
-		return "public/registro.jsp";
-	}
-
->>>>>>> branch 'dev' of https://github.com/sheidiz/CodingDojo-Project.git
 	@GetMapping("/nosotros")
 	public String nosotros(Model model) {
 		model.addAttribute("userInSession", usuarioProvisorio);
